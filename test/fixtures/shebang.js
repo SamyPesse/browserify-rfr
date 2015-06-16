478841285f2523b4c228c9aca8c642299cbf8423
Samy Pesse <samypesse@gmail.com>
2015-06-16 16:18:43 +0200

    Handle shebang

---
+++ test/fixtures/shebang.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+
+require('rfr').setRoot(__dirname);
+
+module.exports = rfr('t1/test');
--- test/rfr.js
+++ test/rfr.js
@@ -5,14 +5,34 @@ var browserify = require('browserify');
 var rfr = require('../');
 
 describe('browserify-rfr', function() {
+    var fixtures = path.resolve(__dirname, 'fixtures');
+
     it('should correctly replaced "rfr()" calls', function(done) {
-        var fixtures = path.resolve(__dirname, 'fixtures');
         var b = browserify();
 
         b.transform(rfr, {
             root: fixtures
         })
         .add(path.resolve(fixtures, 'main.js'));
+
+        b.bundle(function(err, data) {
+            if (err) return done(err);
+            data = data.toString();
+
+            expect(data).to.contain('module.exports = 1;');
+            expect(data).to.contain('module.exports = require("./../t2");');
+
+            done();
+        });
+    });
+
+    it('should correctly handle file starting with shebang', function(done) {
+        var b = browserify();
+
+        b.transform(rfr, {
+            root: fixtures
+        })
+        .add(path.resolve(fixtures, 'shebang.js'));
 
         b.bundle(function(err, data) {
             if (err) return done(err);
