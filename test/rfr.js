64636c44e431b688680375f03f38b8ac8629dbf5
Samy Pesse <samypesse@gmail.com>
2015-06-16 15:15:35 +0200

    Complete base test

--- test/rfr.js
+++ test/rfr.js
@@ -1,4 +1,4 @@
-var chai = require('chai');
+var expect = require('chai').expect;
 var path = require('path');
 var browserify = require('browserify');
 
@@ -8,12 +8,21 @@ describe('browserify-rfr', function() {
     it('should correctly replaced "rfr()" calls', function(done) {
         var fixtures = path.resolve(__dirname, 'fixtures');
         var b = browserify();
+
         b.transform(rfr, {
             root: fixtures
+        })
+        .add(path.resolve(fixtures, 'main.js'));
+
+        b.bundle(function(err, data) {
+            if (err) return done(err);
+            data = data.toString();
+
+            expect(data).to.contain('module.exports = 1;');
+            expect(data).to.contain('module.exports = require("./../t2");');
+
+            done();
         });
-        b.add(path.resolve(fixtures, 'main.js'));
-        b.bundle()
-        .pipe(process.stdout);
     });
 });
 
